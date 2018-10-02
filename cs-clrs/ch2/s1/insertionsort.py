2af83f71cb1fbc8dda1bdab2cdef978653be31c3
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-10-01 22:19:37 -0400

    Implement insertion sort in descending order

--- cs-clrs/ch2/s1/insertionsort.py
+++ cs-clrs/ch2/s1/insertionsort.py
@@ -9,11 +9,26 @@ def insertionSort(arr):
         while i >= 0 and arr[i] > key:
             arr[i + 1] = arr[i]
             i -= 1
-        print(i)
         arr[i + 1] = key
+    return arr
+
+
+def insertionSortDescending(arr):
+
+    for j in range(len(arr) - 2, -1, -1):
+        key = arr[j]
+        i = j + 1
+        while i < len(arr) and arr[i] > key:
+            arr[i - 1] = arr[i]
+            i += 1
+        arr[i - 1] = key
     return arr
 
 
 if __name__ == "__main__":
     arr = [5, 2, 4, 6, 1, 3]
-    print(insertionSort(arr))
+    # print(insertionSort(arr))
+    print(insertionSortDescending(arr))
+
+    arr = [31, 41, 59, 26, 41, 58]
+    print(insertionSortDescending(arr))
