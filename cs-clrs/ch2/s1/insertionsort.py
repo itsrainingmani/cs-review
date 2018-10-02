b1b3e53da2f7aceff8f97027b94001f72662ffd0
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-10-01 22:04:38 -0400

    Fix Insertion sort

--- cs-clrs/ch2/s1/insertionsort.py
+++ cs-clrs/ch2/s1/insertionsort.py
@@ -6,9 +6,10 @@ def insertionSort(arr):
     for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
-        while i > 0 and arr[i] > key:
+        while i >= 0 and arr[i] > key:
             arr[i + 1] = arr[i]
             i -= 1
+        print(i)
         arr[i + 1] = key
     return arr
 
