80a66509a59f54b851107475623a09d4ef7f586e
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-10-21 17:20:12 -0400

    Fix the remove algorithm

--- basecs/heap.py
+++ basecs/heap.py
@@ -13,9 +13,26 @@ class MinHeap:
 
     def min_heapify(self):
         i = self.count - 1
-        while i > 0 and self.heap[i] < self.heap[(i - 1) // 2]:
-            self.swap(i, (i - 1) // 2)
-            i = (i - 1) // 2
+        while i > 0 and self.heap[i] < self.heap[self.get_parent_index(i)]:
+            self.swap(i, self.get_parent_index(i))
+            i = self.get_parent_index(i)
+
+    def filter_down(self, index):
+        min_index = 0
+        left, right = self.get_children_indices(index)
+        if right >= len(self.heap):
+            if left >= len(self.heap):
+                return
+            else:
+                min_index = left
+        else:
+            if self.heap[left] <= self.heap[right]:
+                min_index = left
+            else:
+                min_index = right
+        if self.heap[index] > self.heap[min_index]:
+            self.swap(min_index, index)
+            self.filter_down(min_index)
 
     def swap(self, i1, i2):
         temp = self.heap[i1]
@@ -26,34 +43,23 @@ class MinHeap:
     def remove(self, value):
         try:
             index = self.heap.index(value)
-            left, right = self.get_children_indices(index)
         except ValueError:
             print("The value does not exist in the heap")
             return False
-        self.count -= 1
-        self.heap[index] = self.heap[self.count]
 
-        while (
-            left < self.count
-            and self.heap[index] > self.heap[left]
-            or self.heap[index] > self.heap[right]
-        ):
-            if self.heap[left] < self.heap[right]:
-                self.swap(left, index)
-                index = left
-            else:
-                self.swap(right, index)
-                index = right
+        self.heap[index] = self.heap[self.count - 1]
+        self.heap.pop()
+        self.count -= 1
+        self.filter_down(index)
+
         return True
 
     def contains(self, value):
         start = 0
         nodes = 1
 
-        
-
     def get_parent_index(self, cur_index):
-        return (index - 1) / 2
+        return (cur_index - 1) // 2
 
     def get_children_indices(self, cur_index):
         return 2 * cur_index + 1, 2 * cur_index + 2
@@ -62,9 +68,9 @@ class MinHeap:
 if __name__ == "__main__":
     mh = MinHeap()
 
-    for i in [3, 9, 12, 7, 1]:
+    for i in [3, 9, 12, 13, 1]:
         mh.add(i)
         print(mh)
 
-    # mh.remove(9)
+    mh.remove(1)
     print(mh)
